c59bc6d3640a4fc1489502382671b4c485f671b3
Beth Williams <BWilliams@intera.com>
2016-11-09 16:48:46 -0800

    Just scratching the surface of looking for stomp runs on olive and green

--- stomp/Beth/Task1.py
+++ stomp/Beth/Task1.py
@@ -1,1 +1,25 @@
+"""
+Task1.py
+-----------
+Utilities for retrieving stomp model runs on \\green and \\olive
 
+written for python 3.4 and up
+
+
+tests are in test_stomp.py
+"""
+import os
+
+
+def find_file(target, folder): #target will be filename, folder I am hoping can be working_data.
+
+
+def find_stomp_runs():
+    
+drives = ['\\green', '\\olive']
+
+for drive in drives:
+    if os.path.isdir(drive):
+    filepath = find_file(output, drive)
+    if filepath is not None:
+        break
