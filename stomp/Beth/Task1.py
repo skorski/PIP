8084e536cb4a7679292ebf6d50f0352fcbfa324c
Beth Williams <BWilliams@intera.com>
2016-11-10 10:27:38 -0800

    Addded find file method, and got a filepath returned, but only one at this point, and I know there are more.

--- stomp/Beth/Task1.py
+++ stomp/Beth/Task1.py
@@ -10,16 +10,31 @@ tests are in test_stomp.py
 """
 import os
 
+target = "output"
+folder = "working_data"
 
 def find_file(target, folder): #target will be filename, folder I am hoping can be working_data.
-
+    for f in os.listdir(folder):
+        path = os.path.join(folder, f)
+        if os.path.isdir(path):
+            result = find_file(target, path)
+            if result is not None:
+                return result
+            continue
+        if f == target:
+            return path
 
 def find_stomp_runs():
     
-drives = ['\\green', '\\olive']
+    drives = ['Z:\\', 'X:\\']
 
-for drive in drives:
-    if os.path.isdir(drive):
-    filepath = find_file(output, drive)
-    if filepath is not None:
-        break
+    for drive in drives:
+        if os.path.isdir(drive):
+            filepath = find_file('output', drive)
+            return filepath
+            if filepath is not None:
+                break
+            
+        
+if __name__=="__main__":
+    pass
